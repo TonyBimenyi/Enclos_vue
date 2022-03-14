15f19fdf7724fb73f5ad694db6ca4fdc4d06749f
TonyBimenyi <tonyblaisb@gmail.com>
2022-03-15 01:12:16 +0200

    all

---
+++ src/components/add_student.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="container">
+    
+         <div class="wrapper fadeInDown">
+          <div id="formContent">
+            <!-- Tabs Titles -->
+
+            <!-- Icon -->
+            <div class="fadeIn first">
+              <span>Ajouter un eleve</span>
+            </div>
+
+            <!-- Login Form -->
+            
+              <input type="text" id="login" v-model="username" class="fadeIn second" name="login" placeholder="Nom d'eleve">
+              <input type="text" v-model="password" id="password" class="fadeIn third" name="login" placeholder="Prenom">
+              <select name="" id="">
+                  <option disabled>--Selectionner--</option>
+                  <option value="">Fondamental</option>
+                  <option value="">Post Fondamental</option>
+              </select>
+             
+              <input type="text" v-model="password" id="password" class="fadeIn third" name="login" placeholder="Classe">
+              <label for="">Date de Naissance</label>
+              <input type="date" v-model="password" id="password" class="fadeIn third" name="login" placeholder="Date de Naissance">
+             
+              <input class="submit" type="submit" @click="login"  value="AJOUTER">
+            
+
+            <!-- Remind Passowrd -->
+            <div id="formFooter">
+              <a @click="close" class="underlineHover" href="#">Fermer</a>
+            </div>
+
+          </div>
+        </div>
+    </div>
+</template>
+<script>
+    import axios from 'axios'
+export default {
+    methods:{
+        close(){
+            this.$emit('close')
+        },
+    }
+}
+</script>
+<style scoped>
+  .container{
+     background: rgba(0, 0, 0, 0.871);
+     z-index: 1;
+     top:0;
+     position: fixed;
+     display: flex;
+     justify-content: center;
+     width: 100%;
+     height: 100%;
+  }
+  input{
+      height: 40px;
+      /* border-bottom: 2px solid var(--yellow); */
+  }
+  .submit{
+      height: 50px;
+  }
+  input[type=date]{
+    background-color: back;
+    border: none;
+    color: var(--blue);
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 5px;
+    width: 85%;
+    border: 2px solid #f6f6f6;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+    -webkit-border-radius: 5px 5px 5px 5px;
+    border-radius: 5px 5px 5px 5px;
+}
+select{
+    background-color: white;
+    border: none;
+    /* height: 45px; */
+    color: var(--blue);
+    cursor:pointer;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 5px;
+    width: 85%;
+    border: 2px solid #f6f6f6;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+    -webkit-border-radius: 5px 5px 5px 5px;
+    border-radius: 5px 5px 5px 5px;
+}
+label{
+    display: block;
+    color: #778FAC;
+    margin-top:10px;
+}
+input[type="button"], input[type="submit"], input[type="reset"]{
+    margin-bottom: 20px;
+}
+#formFoote{
+    padding: 10px;
+}
+.wrapper{
+    margin-top:30px;
+}
+</style>
--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="container">
-        <p>dfdfd</p>
+        <div class="header_table">
+            <div class="add_btn">
+                <button @click="dialog=true"><span class="material-icons-outlined">add_box</span>
+                Ajouter un eleve</button>
+            </div>
+            <div class="search">
+                <input type="search">
+            </div>
+        </div>
         <div class="table">
             <table>
                 <thead>
                     <tr>
-                        <td>ID</td>
+                        <td>#ID</td>
                         <td>NOM</td>
                         <td>PRENOM</td>
                         <td>SECTION</td>
@@ -14,7 +22,31 @@
                         <td>ACTIONS</td>
                     </tr>
                 </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
+         <Student @close="close" v-if="dialog"/>
     </div>
 </template>
+<script>
+import Student from '../components/add_student.vue'
+export default {
+    components:{
+        Student
+    },
+    data(){
+        return{
+            dialog:false
+        }
+    },
+    methods:{
+         close(){
+            this.dialog = false
+        },
+    }
+}
+</script>
